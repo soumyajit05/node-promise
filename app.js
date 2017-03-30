21208a09f4fb2fcd63884300a0c71e977d8aeae5
jit <jit@jits-iMac.local>
2017-03-30 00:13:03 -0400

    change 1

--- app.js
+++ app.js
@@ -1,24 +1,29 @@
 function asyncPromise() {
     let p = new Promise((resolve, reject) => {
         console.log(`inside Promise`);
-        //resolve();
-        reject(`Unknown Error`);
+        resolve(() => {
+            new Promise((resolve,reject) => {
+                reject(`No Way`);
+            })
+        });
+        //reject(`Unknown Error`);
     });
-    p.then(() => {
-        console.log(`Promise resolved`);
-    },
-        (reason) => {
-            console.log(`Promise rejected due to ${reason}`);
-            return reason;
-        }
-    ).then((value) => {
-        console.log(`Really ${value}`);
-    }
-        ).catch((error) => {
-            console.log(`in catch -  ${error}`);
-        })
-        ;
+    return p;
 }
 
-let p = asyncPromise();
+asyncPromise().then((text) => {
+    console.log(`Promise resolved ${text}`);
+    return `success`;
+},
+    (reason) => {
+        console.log(`Promise rejected due to ${reason}`);
+        return reason;
+    }
+).then((value) => {
+    console.log(`Really ${value}`);
+}
+    ).catch((error) => {
+        console.log(`in catch -  ${error}`);
+    })
+    ;;
 
